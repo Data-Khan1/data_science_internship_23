ee81855b388b3bbbd940fae728391b7d25e9ea5d
Moin Khan <125360000+Data-Khan1@users.noreply.github.com>
2023-02-23 18:49:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.header("My name is Moin Khan. I am a highly motivated data scientist. passionate about building models that fix problems using machine learning. I am a problem solver, team leader, and programmer.")
